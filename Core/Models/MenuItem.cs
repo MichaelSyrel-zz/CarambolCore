891a5e40a604ccb9cb9e10ce4dc5036a92b3f4ac
Michael Syrel <syrelmichael@gmail.com>
2020-03-23 01:34:19 +0200

    Adding layers , connecting to temp DB

---
+++ Core/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    internal interface IMenuItem
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        decimal Price { get; set; }
+
+    }
+}
---
+++ CoreAPI/Controllers/ProcessController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Core;
+
+namespace CoreAPI.Controllers
+{
+    [Route("/api/ProcessController")]
+    public class ProcessController : ApiController
+    {
+        [Route("Process")]
+        [HttpGet]
+        public HttpResponseMessage GetProcessById(int processId)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [Route("ActiveProcesses")]
+        [HttpGet]
+        public HttpResponseMessage GetActiveProcesses()
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
